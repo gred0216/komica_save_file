0cf57c201c0a951c7ae1f1502b087f064fd72d86
gred0216 <likuan1222@gmail.com>
2018-03-04 13:08:20 +0800

    Update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='komica_save_file',
+      version='1.0',
+      description='save files from komica',
+      keywords=('komica'),
+      author='gred0216',
+      author_email='betacrunch12345@gmail.com',
+      packages=['komica_save_file'],
+      )
